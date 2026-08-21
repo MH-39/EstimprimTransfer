81aece629d0e6688904361773a1686a5a6867d77
MH-39 <mickael.houriez@estimprim.fr>
2026-08-21 22:07:30 +0200

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -12,7 +12,7 @@ window.ESTIM_CONFIG = {
 
   // URL de votre application Web Google Apps Script déployée.
   // Exemple : https://script.google.com/macros/s/AKfycb.../exec
-  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbxhi3-krANixJUjBDesBEfdgUTdzxDCktxi7xmdohM5DXev7q569Wiy-RzsjYS0VN2a/exec',
+  APPS_SCRIPT_URL: 'https://script.google.com/macros/s/AKfycbwcbpz70zE_zUMF_2WCYTwG2HWjsXW9l5lbFxbTCsXxMuwu6HsdyDHRYfS-vHmA1HUicw/exec',
 
   // Adresse publique de votre site GitHub Pages / domaine personnalisé.
   // PAS de slash final.
